4e5702dfd5faef3c90baddf8b11c6fa85298dfe8
noah <jeonghogim318@gmail.com>
2023-09-04 17:54:12 +0900

    sec06ch01 - h2데이터베이스 설치

--- src/main/java/com/hello/hellospring/SpringConfig.java
+++ src/main/java/com/hello/hellospring/SpringConfig.java
@@ -1,15 +1,22 @@
 package com.hello.hellospring;
 
 import com.hello.hellospring.domain.Member;
+import com.hello.hellospring.repository.JdbcMemberRepository;
 import com.hello.hellospring.repository.MemberRepository;
 import com.hello.hellospring.repository.MemoryMemberRepository;
 import com.hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+
 @Configuration
 public class SpringConfig {
 
+    public SpringConfig(DataSource dataSource){
+        this.datasource = dataSource;
+    }
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -19,4 +26,10 @@ public class SpringConfig {
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new JdbcMemberRepository(dataSource);
+    }
+
 }
