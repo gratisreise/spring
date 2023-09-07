e89363abbbeb7ce7a6784bec9a613fa261f5f7ab
noah <jeonghogim318@gmail.com>
2023-09-07 18:52:31 +0900

    섹션7 - 롬복과 최신 트렌드

--- src/main/java/hello/core/order/OrderServiecImpl.java
+++ src/main/java/hello/core/order/OrderServiecImpl.java
@@ -3,21 +3,17 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiecImpl implements OrderService{
 
      private  final MemberRepository memberRepository;
      private  final DiscountPolicy discountPolicy;
-
-     public OrderServiecImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-         this.memberRepository =memberRepository;
-         this.discountPolicy = discountPolicy;
-    }
-
 
 
     @Override
