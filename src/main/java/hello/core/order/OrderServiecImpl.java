499749da6f60fe6c5ec85dc33ae20fc4ec9873f9
noah <jeonghogim318@gmail.com>
2023-09-07 18:38:00 +0900

    섹션7 - 생성자 주입을 선택해라!

--- src/main/java/hello/core/order/OrderServiecImpl.java
+++ src/main/java/hello/core/order/OrderServiecImpl.java
@@ -10,19 +10,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiecImpl implements OrderService{
 
-     private  MemberRepository memberRepository;
-     private  DiscountPolicy discountPolicy;
+     private  final MemberRepository memberRepository;
+     private  final DiscountPolicy discountPolicy;
 
-    public OrderServiecImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
+     public OrderServiecImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+         this.memberRepository =memberRepository;
+         this.discountPolicy = discountPolicy;
     }
 
-    @Autowired
-    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy){
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
