8f3f38ccf9363f635b4aa3a95933c7978625a3ad
noah <jeonghogim318@gmail.com>
2023-09-08 13:16:06 +0900

    섹션8 - 애노테이션 @PostConstruct, @PreDestory

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -19,7 +19,7 @@ public class BeanLifeCycleTest {
     @Configuration
     static class LifeCycleConfig{
 
-        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient(){
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("heet://hello-spring.dev");
